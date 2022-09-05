77cc46823e5978338e40dfbf5edf6d6340dcf197
Shyam3097 <97452238+Shyam3097@users.noreply.github.com>
2022-09-05 17:31:38 +0530

    Modified blog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,11 +1,33 @@
 // const { count } = require("console")
+const { request } = require("http");
 const BlogModel = require("../models/blogModel");
 
 const createBlog = async function (req, res) {
-  let data = req.body;
-
-  let savedData = await BlogModel.create(data);
-  res.send({ msg: savedData });
+    try{
+        let data = req.body;
+      
+        let savedData = await BlogModel.create(data);
+        res.status(201).send({ msg: savedData });
+    }catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(400).send({ msg: "Error", error: "invalid request" })
+    }
 };
+
+// const createBook = async function (req, res) {
+//     try {
+//         let data = req.body
+//         console.log(data)
+//         if ( Object.keys(data).length != 0) {
+//             let savedData = await BookModel.create(data)
+//             res.status(201).send({ msg: savedData })
+//         }
+//         else res.status(400).send({ msg: "BAD REQUEST"})
+//     }
+//     catch (err) {
+//         console.log("This is the error :", err.message)
+//         res.status(500).send({ msg: "Error", error: err.message })
+//     }
+
 
 module.exports.createBlog = createBlog;
